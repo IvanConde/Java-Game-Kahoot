29d01f789f9a0f885e3470e57cca9a3e78ccb6da
Nicolás Menese <nmenese@fi.uba.ar>
2020-08-07 15:17:56 -0300

    se borraron clases que quedaron afuera

--- src/main/java/edu/fiuba/algo3/modelo/VyFMasDeDosOpcionesException.java
+++
@@ -1,9 +0,0 @@
-package edu.fiuba.algo3.modelo;
-
-public class VyFMasDeDosOpcionesException extends RuntimeException{
-
-    public VyFMasDeDosOpcionesException(String mensaje){
-        super(mensaje);
-    }
-
-}
