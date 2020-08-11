bc8fc717208e731d3748a81a89dd0ba615027618
Bmorseletto <bmorseletto@fi.uba.ar>
2020-08-11 19:37:16 -0300

    Se Agrego una excepcion para chequear que dos Opciones en OrderedChoice no tengan la misma posicion original

---
+++ src/main/java/edu/fiuba/algo3/modelo/excepciones/OrderedChoiceMasDeUnaOpcionConLaPosicionOriginalException.java
@@ -0,0 +1,7 @@
+package edu.fiuba.algo3.modelo.excepciones;
+
+public class OrderedChoiceMasDeUnaOpcionConLaPosicionOriginalException extends RuntimeException{
+    public OrderedChoiceMasDeUnaOpcionConLaPosicionOriginalException(String mensaje){
+        super(mensaje);
+    }
+}
