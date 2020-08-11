6f732ba5d4799c289ffe2c7408bb272ec7709a4d
Bmorseletto <bmorseletto@fi.uba.ar>
2020-08-10 23:20:36 -0300

    Se Agrego Las pruebas unitarias de Exclusividad, group Ordered y multiple choice, puntaje y jugador

---
+++ src/main/test/edu/fiuba/algo3/modelo/testunitarios/GroupChoiceUnitTest.java
@@ -0,0 +1,25 @@
+package edu.fiuba.algo3.modelo.testunitarios;
+
+import edu.fiuba.algo3.modelo.modalidades.Clasico;
+import edu.fiuba.algo3.modelo.opciones.Opcion;
+import edu.fiuba.algo3.modelo.opciones.OpcionEstructurada;
+import edu.fiuba.algo3.modelo.preguntas.GroupChoice;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GroupChoiceUnitTest {
+
+    @Test
+    public void test01_CrearPreguntaGroupChoice(){
+        ArrayList<Opcion> todasLasOpciones = new ArrayList<Opcion>();
+        OpcionEstructurada opcion1 = new OpcionEstructurada("1", 1);
+        OpcionEstructurada opcion2 = new OpcionEstructurada("0", 0);
+        todasLasOpciones.add(opcion1);
+        todasLasOpciones.add(opcion2);
+        GroupChoice preguntaGroupChoiceTest = new GroupChoice("test", todasLasOpciones, new Clasico());
+        assertNotEquals(null, preguntaGroupChoiceTest);
+    }
+}
