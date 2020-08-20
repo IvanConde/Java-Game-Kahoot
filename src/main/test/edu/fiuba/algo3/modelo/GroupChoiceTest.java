c0b60020167f9d992732a395c8dfe2c83e2cfede
Alvaro0626 <enriquealvaro97@gmail.com>
2020-08-19 22:41:12 -0300

    Se agregaron los tests restantes de exclusividad en orderded y group choice
    Co-authored-by: Ivan Conde <iconde@fi.uba.ar>

--- src/main/test/edu/fiuba/algo3/modelo/GroupChoiceTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/GroupChoiceTest.java
@@ -140,4 +140,117 @@ public class GroupChoiceTest {
 
     }
 
+    @Test
+    public void test06_5ResponderGroupChoiceyAsignarPuntajesCorrectamenteConExclusividadPuntajeActivadaParaUnJugador() {
+
+        //Ordenar en los grupos:
+        //Grupo 0 = numeros primos
+        //Grupo 1 = numeros no primos
+        OpcionGroup opcion1 = new OpcionGroup("2", "0");
+        OpcionGroup opcion2 = new OpcionGroup("8", "1");
+        OpcionGroup opcion3 = new OpcionGroup("13", "0");
+        OpcionGroup opcion4 = new OpcionGroup("12", "1");
+        OpcionGroup opcion5 = new OpcionGroup("17", "0");
+
+        Jugador jugador1 = new Jugador("Ivan");
+        Jugador jugador2 = new Jugador("Enrique");
+
+        ArrayList<Opcion> opciones = new ArrayList<Opcion>();
+
+        opciones.add(opcion1);
+        opciones.add(opcion2);
+        opciones.add(opcion3);
+        opciones.add(opcion4);
+        opciones.add(opcion5);
+
+        ArrayList<Opcion> opcionesJugador1 = new ArrayList<Opcion>();
+        opcion1.elegirGrupo("0", opcionesJugador1);
+        opcion2.elegirGrupo("1", opcionesJugador1);
+        opcion3.elegirGrupo("0", opcionesJugador1);
+        opcion4.elegirGrupo("1", opcionesJugador1);
+        opcion5.elegirGrupo("0", opcionesJugador1);
+
+        ArrayList<Opcion> opcionesJugador2 = new ArrayList<Opcion>();
+        opcion1.elegirGrupo("1", opcionesJugador1);
+        opcion2.elegirGrupo("0", opcionesJugador1);
+        opcion3.elegirGrupo("1", opcionesJugador1);
+        opcion4.elegirGrupo("0", opcionesJugador1);
+        opcion5.elegirGrupo("1", opcionesJugador1);
+
+        Respuesta respuesta1 = new Respuesta(opcionesJugador1, jugador1.puntaje());
+        Respuesta respuesta2 = new Respuesta(opcionesJugador2, jugador2.puntaje());
+
+        ArrayList<Respuesta> respuestas = new ArrayList<Respuesta>();
+        respuestas.add(respuesta1);
+        respuestas.add(respuesta2);
+        ArrayList<String> grupos = new ArrayList<String>();
+        grupos.add("0");
+        grupos.add("1");
+        GroupChoice preguntaTest = new GroupChoice("Colocar en el grupo 0 los numeros primos y en el grupo 1 los no-primos", opciones, grupos);
+
+        preguntaTest.usarExclusividad(jugador2);
+        preguntaTest.responderPregunta(respuestas);
+
+        assertEquals(0, jugador1.puntaje().getPuntaje());
+        assertEquals(2, jugador2.puntaje().getPuntaje());
+
+    }
+
+    @Test
+    public void test06_5ResponderGroupChoiceyAsignarPuntajesCorrectamenteConExclusividadPuntajeActivadaParaLosDosJugadores() {
+
+        //Ordenar en los grupos:
+        //Grupo 0 = numeros primos
+        //Grupo 1 = numeros no primos
+        OpcionGroup opcion1 = new OpcionGroup("2", "0");
+        OpcionGroup opcion2 = new OpcionGroup("8", "1");
+        OpcionGroup opcion3 = new OpcionGroup("13", "0");
+        OpcionGroup opcion4 = new OpcionGroup("12", "1");
+        OpcionGroup opcion5 = new OpcionGroup("17", "0");
+
+        Jugador jugador1 = new Jugador("Ivan");
+        Jugador jugador2 = new Jugador("Enrique");
+
+        ArrayList<Opcion> opciones = new ArrayList<Opcion>();
+
+        opciones.add(opcion1);
+        opciones.add(opcion2);
+        opciones.add(opcion3);
+        opciones.add(opcion4);
+        opciones.add(opcion5);
+
+        ArrayList<Opcion> opcionesJugador1 = new ArrayList<Opcion>();
+        opcion1.elegirGrupo("0", opcionesJugador1);
+        opcion2.elegirGrupo("1", opcionesJugador1);
+        opcion3.elegirGrupo("0", opcionesJugador1);
+        opcion4.elegirGrupo("1", opcionesJugador1);
+        opcion5.elegirGrupo("0", opcionesJugador1);
+
+        ArrayList<Opcion> opcionesJugador2 = new ArrayList<Opcion>();
+        opcion1.elegirGrupo("1", opcionesJugador1);
+        opcion2.elegirGrupo("0", opcionesJugador1);
+        opcion3.elegirGrupo("1", opcionesJugador1);
+        opcion4.elegirGrupo("0", opcionesJugador1);
+        opcion5.elegirGrupo("1", opcionesJugador1);
+
+        Respuesta respuesta1 = new Respuesta(opcionesJugador1, jugador1.puntaje());
+        Respuesta respuesta2 = new Respuesta(opcionesJugador2, jugador2.puntaje());
+
+        ArrayList<Respuesta> respuestas = new ArrayList<Respuesta>();
+        respuestas.add(respuesta1);
+        respuestas.add(respuesta2);
+        ArrayList<String> grupos = new ArrayList<String>();
+        grupos.add("0");
+        grupos.add("1");
+        GroupChoice preguntaTest = new GroupChoice("Colocar en el grupo 0 los numeros primos y en el grupo 1 los no-primos", opciones, grupos);
+
+        preguntaTest.usarExclusividad(jugador1);
+        preguntaTest.usarExclusividad(jugador2);
+        preguntaTest.responderPregunta(respuestas);
+
+        assertEquals(0, jugador1.puntaje().getPuntaje());
+        assertEquals(4, jugador2.puntaje().getPuntaje());
+
+    }
+
 }
--- src/main/test/edu/fiuba/algo3/modelo/OrderedChoiceTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/OrderedChoiceTest.java
@@ -1,6 +1,9 @@
 package edu.fiuba.algo3.modelo;
+import edu.fiuba.algo3.modelo.modalidades.ModalidadClasico;
 import edu.fiuba.algo3.modelo.opciones.Opcion;
+import edu.fiuba.algo3.modelo.opciones.OpcionBooleana;
 import edu.fiuba.algo3.modelo.opciones.OpcionOrdered;
+import edu.fiuba.algo3.modelo.preguntas.MultipleChoice;
 import edu.fiuba.algo3.modelo.preguntas.OrderedChoice;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
@@ -128,5 +131,109 @@ public class OrderedChoiceTest {
         assertEquals(1, jugador2.puntaje().getPuntaje());
 
     }
+
+    @Test
+    public void test06_4UnJugadorRespondeOrderedChoiceySeAsignaPuntajesCorrectamenteConExclusividadPuntaje(){
+
+        //Ordenar de mayor a menor
+        OpcionOrdered opcion1 = new OpcionOrdered("4",0);
+        OpcionOrdered opcion2 = new OpcionOrdered("7",1);
+        OpcionOrdered opcion3 = new OpcionOrdered("9",2);
+        OpcionOrdered opcion4 = new OpcionOrdered("10",3);
+        OpcionOrdered opcion5 = new OpcionOrdered("25",4);
+
+        Jugador jugador1 = new Jugador("Ivan");
+        Jugador jugador2 = new Jugador("Enrique");
+
+        ArrayList<Opcion> opciones = new ArrayList<Opcion>();
+        opciones.add(opcion1);
+        opciones.add(opcion2);
+        opciones.add(opcion3);
+        opciones.add(opcion4);
+        opciones.add(opcion5);
+
+        ArrayList<Opcion> opcionesJugador1 = new ArrayList<Opcion>();
+        opcion1.elegirPosicion(0, opcionesJugador1);
+        opcion2.elegirPosicion(1, opcionesJugador1);
+        opcion3.elegirPosicion(3, opcionesJugador1);
+        opcion4.elegirPosicion(2, opcionesJugador1);
+        opcion5.elegirPosicion(4, opcionesJugador1);
+
+        ArrayList<Opcion> opcionesJugador2 = new ArrayList<Opcion>();
+        opcion1.elegirPosicion(0, opcionesJugador2);
+        opcion2.elegirPosicion(1, opcionesJugador2);
+        opcion3.elegirPosicion(2, opcionesJugador2);
+        opcion4.elegirPosicion(3, opcionesJugador2);
+        opcion5.elegirPosicion(4, opcionesJugador2);
+
+        Respuesta respuestaJugador1 = new Respuesta(opcionesJugador1,jugador1.puntaje());
+        Respuesta respuestaJugador2 = new Respuesta(opcionesJugador2,jugador2.puntaje());
+
+        ArrayList<Respuesta> respuestas = new ArrayList<Respuesta>();
+        respuestas.add(respuestaJugador1);
+        respuestas.add(respuestaJugador2);
+
+        OrderedChoice preguntaTest = new OrderedChoice("Ordenar de menor a mayor", opciones);
+
+        preguntaTest.usarExclusividad(jugador2);
+        preguntaTest.responderPregunta(respuestas);
+
+        assertEquals(0,jugador1.puntaje().getPuntaje());
+        assertEquals(2, jugador2.puntaje().getPuntaje());
+
+    }
+
+    @Test
+    public void test06_5ResponderOrderedChoiceyAsignarPuntajesCorrectamenteConExclusividadPuntajeActivadaParaLosDosJugadores(){
+
+        //Ordenar de mayor a menor
+        OpcionOrdered opcion1 = new OpcionOrdered("4",0);
+        OpcionOrdered opcion2 = new OpcionOrdered("7",1);
+        OpcionOrdered opcion3 = new OpcionOrdered("9",2);
+        OpcionOrdered opcion4 = new OpcionOrdered("10",3);
+        OpcionOrdered opcion5 = new OpcionOrdered("25",4);
+
+        Jugador jugador1 = new Jugador("Ivan");
+        Jugador jugador2 = new Jugador("Enrique");
+
+        ArrayList<Opcion> opciones = new ArrayList<Opcion>();
+        opciones.add(opcion1);
+        opciones.add(opcion2);
+        opciones.add(opcion3);
+        opciones.add(opcion4);
+        opciones.add(opcion5);
+
+        ArrayList<Opcion> opcionesJugador1 = new ArrayList<Opcion>();
+        opcion1.elegirPosicion(0, opcionesJugador1);
+        opcion2.elegirPosicion(1, opcionesJugador1);
+        opcion3.elegirPosicion(3, opcionesJugador1);
+        opcion4.elegirPosicion(2, opcionesJugador1);
+        opcion5.elegirPosicion(4, opcionesJugador1);
+
+        ArrayList<Opcion> opcionesJugador2 = new ArrayList<Opcion>();
+        opcion1.elegirPosicion(0, opcionesJugador2);
+        opcion2.elegirPosicion(1, opcionesJugador2);
+        opcion3.elegirPosicion(2, opcionesJugador2);
+        opcion4.elegirPosicion(3, opcionesJugador2);
+        opcion5.elegirPosicion(4, opcionesJugador2);
+
+        Respuesta respuestaJugador1 = new Respuesta(opcionesJugador1,jugador1.puntaje());
+        Respuesta respuestaJugador2 = new Respuesta(opcionesJugador2,jugador2.puntaje());
+
+        ArrayList<Respuesta> respuestas = new ArrayList<Respuesta>();
+        respuestas.add(respuestaJugador1);
+        respuestas.add(respuestaJugador2);
+
+        OrderedChoice preguntaTest = new OrderedChoice("Ordenar de menor a mayor", opciones);
+
+        preguntaTest.usarExclusividad(jugador1);
+        preguntaTest.usarExclusividad(jugador2);
+        preguntaTest.responderPregunta(respuestas);
+
+        assertEquals(0,jugador1.puntaje().getPuntaje());
+        assertEquals(4, jugador2.puntaje().getPuntaje());
+
+    }
+
 
 }
